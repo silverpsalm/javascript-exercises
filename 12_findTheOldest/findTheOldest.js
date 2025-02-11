736b3e6e131fe68baea8badaf1559e9efe1034de
SilverPsalm <171736682+silverpsalm@users.noreply.github.com>
2025-02-11 08:35:27 -0500

    Create function to find the oldest person in array
    Create function to find oldest person in array of
    objects with sort method. Method of sorting is
    comparison of the difference between birth and
    death year values of each person's object.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+  arr.sort((a,b) => (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth));
+ 
+  return arr[arr.length-1];
 };
+
+// const people = [
+//     {
+//       name: "Carly",
+//       yearOfBirth: 1942,
+//       yearOfDeath: 1970,
+//     },
+//     {
+//       name: "Ray",
+//       yearOfBirth: 1962,
+//       yearOfDeath: 2011,
+//     },
+//     {
+//       name: "Jane",
+//       yearOfBirth: 1912,
+//       yearOfDeath: 1941,
+//     },
+//   ]
+
+//   findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
