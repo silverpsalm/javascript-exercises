ece0be04a657221b6a85ea706c90ee1fce68063a
SilverPsalm <171736682+silverpsalm@users.noreply.github.com>
2025-02-11 08:57:06 -0500

    Fix sort method to accomodate no death date value
    If no death date is available, assigns current
    year in place of that value to compare lifespans.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,29 +1,44 @@
 const findTheOldest = function(arr) {
 
-  arr.sort((a,b) => (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth));
+  arr.sort((a,b) => {
+
+    const currentYear = new Date().getFullYear();
+    let aDeathYear = a.yearOfDeath;
+    let bDeathYear = b.yearOfDeath;
+
+    console.log(currentYear);
+
+    if (!a.yearOfDeath) {
+      aDeathYear = currentYear;
+    } else if (!b.yearOfDeath) {
+      bDeathYear = currentYear;
+    }
+
+    return ((aDeathYear - a.yearOfBirth) - (bDeathYear - b.yearOfBirth));
+
+  });
  
   return arr[arr.length-1];
 };
 
-// const people = [
-//     {
-//       name: "Carly",
-//       yearOfBirth: 1942,
-//       yearOfDeath: 1970,
-//     },
-//     {
-//       name: "Ray",
-//       yearOfBirth: 1962,
-//       yearOfDeath: 2011,
-//     },
-//     {
-//       name: "Jane",
-//       yearOfBirth: 1912,
-//       yearOfDeath: 1941,
-//     },
-//   ]
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
 
-//   findTheOldest(people);
+  findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
